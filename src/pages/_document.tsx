500cbd38065133c2381ff11281479c7d2d98f06e
ciocan <ciocan@gmail.com>
2021-03-25 13:48:22 +0000

    add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,11 @@ export default class MyDocument extends Document {
           <link rel="preload" href="/fonts/Manrope-SemiBold.ttf" as="font" crossOrigin="" />
           <link rel="preload" href="/fonts/Manrope-Bold.ttf" as="font" crossOrigin="" />
 
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+
           <script
             type="text/javascript"
             src={`https://maps.google.com/maps/api/js?key=${GCP_MAPS_KEY}&libraries=places`}
