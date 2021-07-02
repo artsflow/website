4521b2974b54ebe132a649f6d61e9527b90ddc84
ciocan <ciocan@gmail.com>
2021-07-02 11:51:52 +0100

    add word-break break-all for description

--- src/components/ActivityPage/components/Details.tsx
+++ src/components/ActivityPage/components/Details.tsx
@@ -12,6 +12,8 @@ export const Description = ({ text }: any) => {
     <VStack
       alignItems="flex-start"
       pb="1rem"
+      wordBreak="break-all"
+      // wordWrap="break-word"
       sx={{
         h3: { fontWeight: 'semibold', fontSize: 'lg' },
         'ul, ol': { pl: '2rem' },
