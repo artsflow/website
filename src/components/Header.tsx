ea7f971775150b0fb17075ae910796fe4c988c90
ciocan <ciocan@gmail.com>
2021-07-09 16:31:13 +0100

    why hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,33 +6,56 @@ import Logo from 'svg/artsflow.svg'
 
 import { ARTSFLOW_APP_URL } from 'lib/config'
 
-export const Header = () => (
-  <HStack as="nav" h="100px" bg="#edf8fa" w="full" justifyContent="space-between" px="2rem">
-    <NextLink href="/">
-      <Link>
-        <Logo width="107px" height="24px" />
-      </Link>
-    </NextLink>
-    <HStack display={['none', 'none', 'flex']} spacing="3rem">
-      <MenuLink href="/creative-corner" title="Creative Corner" />
-      <MenuLink href="/why" title="Why Artsflow?" />
-      <MenuLink href="/about" title="About Us" />
-      <Link
-        isExternal
-        href={ARTSFLOW_APP_URL}
-        color="white"
-        bg="af.teal"
-        px="1rem"
-        py="0.5rem"
-        rounded="8px"
-      >
-        <Text fontWeight="bold">Get Started</Text>
-      </Link>
-    </HStack>
-  </HStack>
-)
+interface BgMap {
+  [key: string]: string
+}
 
-const MenuLink = ({ href, title }: any) => {
+const bg: BgMap = {
+  '/why': '#fff9e7',
+}
+
+export const Header = () => {
+  const { route } = useRouter()
+  return (
+    <HStack
+      as="nav"
+      h="100px"
+      bg={bg[route] || '#edf8fa'}
+      w="full"
+      justifyContent="space-between"
+      px="2rem"
+    >
+      <NextLink href="/">
+        <Link>
+          <Logo width="107px" height="24px" />
+        </Link>
+      </NextLink>
+      <HStack display={['none', 'none', 'flex']} spacing="3rem">
+        <MenuLink href="/creative-corner" title="Creative Corner" />
+        <MenuLink href="/why" title="Why Artsflow?" />
+        <MenuLink href="/about" title="About Us" />
+        <Link
+          isExternal
+          href={ARTSFLOW_APP_URL}
+          color="white"
+          bg="af.teal"
+          px="1rem"
+          py="0.5rem"
+          rounded="8px"
+        >
+          <Text fontWeight="bold">Get Started</Text>
+        </Link>
+      </HStack>
+    </HStack>
+  )
+}
+
+interface MenuLinkProps {
+  href: string
+  title: string
+}
+
+const MenuLink = ({ href, title }: MenuLinkProps) => {
   const { route } = useRouter()
   const selectedLine = {
     content: '""',
