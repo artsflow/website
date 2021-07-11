95382413de99e5018b56ac192b477e13b8cca26d
ciocan <ciocan@gmail.com>
2021-07-11 11:53:56 +0100

    add webminar videos to creative corner

--- src/pages/creative-corner.tsx
+++ src/pages/creative-corner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { VStack, Stack, Text, Heading, Box } from '@chakra-ui/react'
 import Image from 'next/image'
+import ReactPlayer from 'react-player'
 
 import { Meta } from 'components'
 import HeroImg from '../../public/img/hero-cc.webp'
@@ -60,9 +61,93 @@ export default function CreativeCorner(): JSX.Element {
           </Box>
         </Stack>
       </VStack>
-      <VStack minH="600px" justifyContent="center">
-        <Text>rest of content goes here</Text>
+      <VStack
+        justifyContent="center"
+        maxW="1000px"
+        px="2rem"
+        py={['8rem', '12rem']}
+        m="auto"
+        spacing="8rem"
+      >
+        <Webminar
+          url="https://www.youtube.com/watch?v=D6VKhHMXSow"
+          title="How To CONNECT With Your Community"
+        >
+          <Text>
+            'How to connect with your community' is the 1st Creative Corner webinar in our series
+            exploring how you can CONNECT with your community and restart your creative practice. 🙌
+          </Text>
+          <Text>
+            <b>In the webinar we cover:</b>
+            <br />
+            ✔️ How to find & contact various types of client.
+            <br />
+            ✔️ How to market yourself locally.
+            <br />
+            ✔️ How to plant those seeds & build lasting relationships.
+            <br />
+            ✔️ We share our experiences and insights over the last 9 years with Creative Minds.
+            <br />
+            ✔️ And more!
+          </Text>
+        </Webminar>
+        <Webminar
+          url="https://www.youtube.com/watch?v=wvEda8JoaYA"
+          title="Branding - How To Get Started?"
+        >
+          <Text>
+            'Branding - How to get started?' is the 2nd Creative Corner webinar in our series
+            exploring the importance of branding and how to craft your BRAND for your creative
+            business. 🙌
+          </Text>
+          <Text>
+            <b>In the webinar we cover:</b>
+            <br />
+            ✔️ What is Branding? - It's more than just a logo!
+            <br />
+            ✔️ The brand values, brand positioning, fonts, colours and logo.
+            <br />
+            ✔️ How branding can shape the success of your creative business.
+            <br />
+            ✔️ What to focus on (depending on what stage you're at in your creative journey).
+            <br />
+            ✔️ And more!
+          </Text>
+        </Webminar>
       </VStack>
     </>
+  )
+}
+
+const Webminar = ({ url, title, children }: any) => {
+  const handlePlayVideo = () => {
+    console.log('handlePlayVideo')
+  }
+
+  const handleEndVideo = () => {
+    console.log('handleEndVideo')
+  }
+
+  return (
+    <VStack w="full" maxW="800px" spacing="1rem" alignItems="flex-start">
+      <Box
+        w={['full', 'full', '800px']}
+        h={['180px', '450px']}
+        bg="teal.100"
+        pos="relative"
+        mt="-4rem"
+      >
+        <ReactPlayer
+          url={url}
+          controls
+          width="100%"
+          height="100%"
+          onPlay={handlePlayVideo}
+          onEnded={handleEndVideo}
+        />
+      </Box>
+      <Heading size="lg">{title}</Heading>
+      {children}
+    </VStack>
   )
 }
