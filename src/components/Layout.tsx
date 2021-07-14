35c4d0f914f2d831c7fbf718a04babd0b314f85f
ciocan <ciocan@gmail.com>
2021-07-14 19:28:45 +0100

    layout update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useContext } from 'react'
 import { Box, VStack } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 import { UserContext } from 'lib/context'
 import { Header } from './Header'
@@ -8,6 +9,7 @@ import { Meta } from './Meta'
 
 export function Layout({ children }: any) {
   const { user } = useContext(UserContext)
+  const { route } = useRouter()
 
   useEffect(() => {
     if (user) window.analytics?.identify(user.id)
@@ -16,13 +18,19 @@ export function Layout({ children }: any) {
   return (
     <>
       <Meta />
-      <VStack minH="100vh" justifyContent="space-between" spacing="0">
-        <Header />
-        <Box as="main" minH="100px" w="full" flex="auto">
-          {children}
-        </Box>
-        <Footer />
-      </VStack>
+      {route === '/a/[...slug]' ? (
+        children
+      ) : (
+        <>
+          <VStack minH="100vh" justifyContent="space-between" spacing="0">
+            <Header />
+            <Box as="main" minH="100px" w="full" flex="auto">
+              {children}
+            </Box>
+            <Footer />
+          </VStack>
+        </>
+      )}
     </>
   )
 }
